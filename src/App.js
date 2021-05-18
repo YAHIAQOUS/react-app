032ae2e4e6ebeeda748b8109199c3609cde63d0f
Yahia Qous <yahiaqous@gmail.com>
2021-05-18 14:31:30 +0300

    lab03 test

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+import data from './components/data.json'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data
+    }
+  }
+
   render() {
     return (
       <div>
+
+
         < Header />
-        < Main />
+        < Main data={this.state.data}/ >
         < Footer />
       </div>
     )
